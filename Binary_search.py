46203d960f32e75ead1e67feb5211b64a791fb44
sayantan saha <sayantansaha320@gmail.com>
2022-06-06 21:53:38 +0530

    first step1

--- Binary_search.py
+++ Binary_search.py
@@ -1,1 +1,8 @@
-print("this is a binary search")
+def linear_search(nList,nNum):
+    for index,element in enumerate(nList):
+        if nNum==element:
+            return index
+    return False
+nList=[12,4,5,232,1,51,67645,3,111,35]
+nNum=51
+print(linear_search(nList,nNum))
