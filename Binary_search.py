afc388053cd8f0416a3e6f42a578bc105d13a790
sayantan saha <sayantansaha320@gmail.com>
2022-06-08 20:46:09 +0530

    initial commit

--- Binary_search.py
+++ Binary_search.py
@@ -1,7 +1,17 @@
 def binary_search(nList,nSearch):
-    a_index=0
-    b_index=len(nList)-1
+    left_index=0
+    right_index=len(nList)-1
     mid_index=0
-    while a_index<b_index:
-       mid_index=(a_index+b_index)//2
-       
+    while left_index<=right_index:
+       mid_index=(left_index+right_index)//2
+       mid_number=nList[mid_index]
+       if mid_number==nSearch:
+           return mid_index
+       if mid_number<nSearch:
+           left_index=mid_index+1
+       else:
+           right_index=mid_index-1
+    return -1
+arr=[21,34,65,686,4534,24566]
+search=4534
+print(binary_search(arr,search))
