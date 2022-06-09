11f95710b66449354c9e33ff319b2055bad5d37c
sayantan saha <sayantansaha320@gmail.com>
2022-06-09 11:35:48 +0530

    initial commit

--- binary_tree_traversal.py
+++ binary_tree_traversal.py
@@ -1,8 +1,33 @@
 class Binary_tree:
     def __init__(self,val):
         self.val=val
-        self.rchild=None
-        self.lchild=None
-root=Binary_tree(12)
-print(root.val)
-print(root.rchild)
+        self.right=None
+        self.left=None
+def inorder(Node):
+    if Node:
+        inorder(Node.left)
+        print(Node.val,end=" ")
+        inorder(Node.right)
+def postorder(Node):
+    if Node:
+        postorder(Node.left)
+        postorder(Node.right)
+        print(Node.val,end=" ")
+def preorder(Node):
+    if Node:
+        print(Node.val,end=" ")
+        preorder(Node.left)
+        preorder(Node.right)
+root = Binary_tree(1)
+root.left = Binary_tree(2)
+root.right= Binary_tree(3)
+root.left.left = Binary_tree(4)
+root.left.right = Binary_tree(5)
+root.right.right=Binary_tree(6)
+root.right.right.right= Binary_tree(7)
+print("tree traversal for inorder:")
+inorder(root)
+print("\npostorder traversal: ")
+postorder(root)
+print("\npreorder traversal: ")
+preorder(root)
